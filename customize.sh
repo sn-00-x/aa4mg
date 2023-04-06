688fe294bef73cd7122a5f7258beb6c24150cf1c
Rikj000 <rik_helsen@hotmail.com>
2023-04-06 18:59:32 +0200

    ✨ Added `phenotype.db` + CoolWalk support

--- customize.sh
+++ customize.sh
@@ -1,1 +1,4 @@
 ui_print "Installing aa4mg..."
+
+cp $MODPATH/data/data/com.google.android.gms/databases/phenotype.db /data/data/com.google.android.gms/databases/phenotype.db
+cp $MODPATH/data/data/com.google.android.projection.gearhead/files/phenotype/shared/com.google.android.projection.gearhead.pb /data/data/com.google.android.projection.gearhead/files/phenotype/shared/com.google.android.projection.gearhead.pb
