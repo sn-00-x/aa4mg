8dccce9aa9aac12ecded3a09becc45e12148313b
Rikj000 <rik_helsen@hotmail.com>
2023-04-13 21:54:24 +0200

    ⚡ Updated `customize.sh` - Implemented `chooseport` - Choose which `stubs` to install - Reworked installation logs

--- customize.sh
+++ customize.sh
@@ -22,7 +22,58 @@ chooseport() {
     return 1 # 1 = False, why shell why..
 }
 
-ui_print "Installing aa4mg..."
+# Initialize installation
+ui_print ""
+ui_print "Installing AA dependency stubs for MicroG"
+ui_print "+ XLauncher Unlocked..."
+ui_print ""
+ui_print " Volume UP (+) = YES"
+ui_print " Volume DOWN (-) = NO"
+
+# Request if Fake Google Maps stub dependency should be installed
+ui_print ""
+ui_print "Install Fake Google Maps stub?"
+ui_print "(a.k.a. com.google.android.apps.maps)"
+ui_print ""
+if chooseport 0; then # 0 = True, why shell why..
+    ui_print "Fake Google Maps stub will be installed on next reboot!"
+else
+    rm -r "$MODPATH/system/app/Maps";
+    ui_print "Excluded Fake Google Maps stub from installation..."
+fi
+
+# Request if Fake Google Speech Services stub dependency should be installed
+ui_print ""
+ui_print "Install Fake Google Speech Services stub?"
+ui_print "(a.k.a. com.google.android.tts)"
+ui_print ""
+if chooseport 0; then # 0 = True, why shell why..
+    ui_print "Fake Google Speech Services stub will be installed on next reboot!"
+else
+    rm -r "$MODPATH/system/app/GoogleTTS";
+    ui_print "Excluded Fake Google Speech Services stub from installation..."
+fi
+
+# Request if Fake Google Search stub dependency should be installed
+ui_print ""
+ui_print "Install Fake Google Search stub?"
+ui_print "(a.k.a. com.google.android.googlequicksearchbox)"
+ui_print ""
+if chooseport 0; then # 0 = True, why shell why..
+    ui_print "Fake Google Search stub will be installed on next reboot!"
+else
+    rm -r "$MODPATH/system/app/Velvet";
+    ui_print "Excluded Fake Google Search stub from installation..."
+fi
 
 cp $MODPATH/data/data/com.google.android.gms/databases/phenotype.db /data/data/com.google.android.gms/databases/phenotype.db
 cp $MODPATH/data/data/com.google.android.projection.gearhead/files/phenotype/shared/com.google.android.projection.gearhead.pb /data/data/com.google.android.projection.gearhead/files/phenotype/shared/com.google.android.projection.gearhead.pb
+
+# Finish installation, dependency APKs will automatically be installed on reboot
+ui_print ""
+ui_print "Installation of AA dependency stubs for MicroG"
+ui_print "+ XLauncher Unlocked successfull! 🎉"
+ui_print ""
+ui_print "Please re-boot your device,"
+ui_print "and then install the actual Android Auto app!"
+ui_print ""
